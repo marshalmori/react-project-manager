bdb0313f3d9cafc824d0a2bcdbbadefbe275324b
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2025-06-05 21:49:07 +0200

    Handling project creation and updating the UI

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,11 @@ function App() {
 
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
-
-  console.log(projectsState);
 
   let content;
 
@@ -41,7 +40,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8 ">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
