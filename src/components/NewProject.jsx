8e48745ee153deb2ffe0f4e261e2d06f34c3c336
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2025-06-05 22:38:31 +0200

    Validating user input and showing an error modal via userInperativeHandle

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
 export default function NewProject({ onAdd }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -11,7 +14,14 @@ export default function NewProject({ onAdd }) {
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
-    // validation...
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
 
     onAdd({
       title: enteredTitle,
@@ -21,28 +31,35 @@ export default function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] my-16">
-      <div>
-        <Input type="text" ref={title} label="Title" />
-        <Input ref={description} label="Description" textarea />
-        <Input type="date" ref={dueDate} label="Due Date" />
-      </div>
+    <>
+      <Modal ref={modal} buttonCaption={"Okay"}>
+        <h2>Invalid Input</h2>
+        <p>Oops.. looks like you forgot to entre a value.</p>
+        <p>Please make sure you provide a valid value for every input field.</p>
+      </Modal>
+      <div className="w-[35rem] my-16">
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
 
-      <menu className="flex items-center justify-end gap-3 my-4 mt-16">
-        <li>
-          <button className="px-6 py-2 rounded-md text-stone-900 hover:text-stone-950 font-semibold hover:border hover:border-stone-900">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={handleSave}
-            className="px-6 py-2 rounded-md bg-stone-900 text-stone-50 hover:bg-stone-950 font-semibold"
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-    </div>
+        <menu className="flex items-center justify-end gap-3 my-4 mt-16">
+          <li>
+            <button className="px-6 py-2 rounded-md text-stone-900 hover:text-stone-950 font-semibold hover:border hover:border-stone-900">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSave}
+              className="px-6 py-2 rounded-md bg-stone-900 text-stone-50 hover:bg-stone-950 font-semibold"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+      </div>
+    </>
   );
 }
