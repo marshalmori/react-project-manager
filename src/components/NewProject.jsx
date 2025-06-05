33a91fbadf4c918ffbba085a4c9eba7021c755c5
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2025-06-05 19:56:08 +0200

    Collecting user input with refs and forwarded refs

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,12 +1,31 @@
+import { useRef } from "react";
 import Input from "./Input";
 
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    // validation...
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
   return (
     <div className="w-[35rem] my-16">
       <div>
-        <Input label="Title" />
-        <Input label="Description" textarea />
-        <Input label="Due Date" />
+        <Input type="text" ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input type="date" ref={dueDate} label="Due Date" />
       </div>
 
       <menu className="flex items-center justify-end gap-3 my-4 mt-16">
@@ -16,7 +35,10 @@ export default function NewProject() {
           </button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-900 text-stone-50 hover:bg-stone-950 font-semibold">
+          <button
+            onClick={handleSave}
+            className="px-6 py-2 rounded-md bg-stone-900 text-stone-50 hover:bg-stone-950 font-semibold"
+          >
             Save
           </button>
         </li>
